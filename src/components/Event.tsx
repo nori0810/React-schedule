52292a572e6d33f3f40bd513f2b045549eba4a8d
nori0810 <sb2jb4hmmz@privaterelay.appleid.com>
2026-08-25 22:40:28 +0900

    Update Event.tsx

--- src/components/Event.tsx
+++ src/components/Event.tsx
@@ -4,15 +4,17 @@ type EventProps = {
   date: string
   time: string
   deleteEvent:(id:number)=>void
+  editEvent:(id:number)=>void;
 }
 
-function Event({ id,title, date, time,deleteEvent }: EventProps) {
+function Event({ id,title, date, time,deleteEvent,editEvent}: EventProps) {
   return (
     <div>
       <h3>{title}</h3>
       <p>{date}</p>
       <p>{time}</p>
       <button onClick={()=>deleteEvent(id)}>削除</button>
+      <button onClick={()=>editEvent(id)}>編集</button>
     </div>
   )
 }
